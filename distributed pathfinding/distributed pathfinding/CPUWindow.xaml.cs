fbc5b25ad654979b737450eb0e630d3342c2fa1d
Måns Odstam <modstam@live.com>
2016-04-07 00:10:32 +0200

    Added some basic GUI elements with WPF

---
+++ distributed pathfinding/distributed pathfinding/CPUWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace distributed_pathfinding
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class CPUWindow : Window
+    {
+
+        private Window main_window;
+        
+        public CPUWindow(Window main)
+        {
+            this.main_window = main;
+            WindowStartupLocation = WindowStartupLocation.Manual;
+            InitializeComponent();
+        }
+    }
+}
---
+++ distributed pathfinding/distributed pathfinding/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace distributed_pathfinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private Window CPUWindow;
+
+        public MainWindow()
+        {
+            setupMainWindow();
+            InitializeComponent();
+            Application.Current.MainWindow.Closing += new CancelEventHandler(mainWindowClosing);
+            this.Loaded += new RoutedEventHandler(mainWindowLoaded);
+            
+        }
+
+        public void setupMainWindow()
+        {
+            //Nothing for now
+        }
+
+
+        public void setupCPUWindow()
+        {
+            CPUWindow = new CPUWindow(this);          
+            CPUWindow.Left = Application.Current.MainWindow.Left + Application.Current.MainWindow.ActualWidth; 
+            CPUWindow.Top = Application.Current.MainWindow.Top;
+            CPUWindow.Show();
+        }
+
+        private void mainWindowClosing(object sender, CancelEventArgs e)
+        {   
+            //lets close our CPU-window when we close the main window
+            CPUWindow.Close();
+            Application.Current.Shutdown();
+        }
+
+        private void mainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            setupCPUWindow();
+        }
+   
+
+    }
+}
