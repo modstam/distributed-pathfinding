4442490f81eef4f117f2f692adb27dd25315146b
Måns Odstam <modstam@live.com>
2016-05-03 00:59:23 +0200

    added a synchronization class for map

---
+++ distributed pathfinding/distributed pathfinding/Simulation/MapSync.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Security;
+using System.Diagnostics;
+namespace distributed_pathfinding.Simulation
+{
+    class MapSync
+    {
+        Map map = null;
+        private object lockObject = new object();
+        private bool flag = true;
+
+        public void putProducedMap(Map map)
+        {
+            lock (lockObject)
+            {
+                while (!flag)
+                {
+                    try
+                    {
+                        Monitor.Wait(lockObject);
+                    }
+                    catch(Exception e)
+                    {
+                        Debug.WriteLine(e);
+                    }
+
+                }
+                this.map = map;
+                flag = false;
+                Monitor.Pulse(lockObject);
+               
+            }
+           
+        }
+
+        public Map getProducedMap()
+        {
+            lock (lockObject)
+            {
+                while (flag)
+                {
+                    try
+                    {
+                        Monitor.Wait(lockObject);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.WriteLine(e);
+                    }
+
+                }
+                flag = true;
+                Monitor.Pulse(lockObject);
+                return map;
+            }
+        }
+    }
+}
